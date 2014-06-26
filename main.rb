6d131b5e2fa6e56f4e00faaa1e4d8e68eeb5b8c5
Becky Osiecki <lobeliaushtar@gmail.com>
2014-06-26 17:07:09 -0400

    Assignment: Set name form
    Introduction to Ruby and Web Development - Lesson 3 #18

--- main.rb
+++ main.rb
@@ -3,6 +3,15 @@ require 'sinatra'
 
 set :sessions, true
 
+get '/' do
+  erb :username
+end
 
+post '/username' do
+  session[:user_name] = params[:user_name].capitalize
+  redirect '/game'
+end
 
-
+get '/game' do
+  erb :game
+end

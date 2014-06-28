43597f976178b5d976cdcb214719db1df4c5786e
Becky Osiecki <lobeliaushtar@gmail.com>
2014-06-28 11:46:25 -0400

    Assignments #21-23
    Introduction to Ruby and Web Development - Lesson 3
    ~ Player Turn
    ~Use helpers to calculate total
    ~Use helper to display card image
    
    changed card images = from
    http://en.wikipedia.org/?title=User:Cburnett/GFDL_images/Playing_cards
    changed card cover image = from
    http://en.wikipedia.org/wiki/Sinatra_(software)

--- main.rb
+++ main.rb
@@ -3,6 +3,51 @@ require 'sinatra'
 
 set :sessions, true
 
+helpers do
+  def calculate_total(cards)
+    hand = cards.map { |card| card[1]}
+
+    total = 0
+    hand.each do |c|
+      if c == 'A'
+        total += 11
+      else total += c.to_i == 0 ? 10 : c.to_i
+      end
+    end
+
+    hand.select { |card| card == 'A' }.count.times do
+      break if total <=21
+      total -= 10
+    end
+
+    total
+  end
+
+  def card_image(card)
+    suit = case card[0]
+      when 'C' then 'club'
+      when 'D' then 'diamond'
+      when 'H' then 'heart'
+      when 'S' then 'spade'
+    end
+
+    value = card[1]
+    if ['J', 'Q', 'K', 'A'].include?(value)
+      value = case card[1]
+        when 'J' then 'jack'
+        when 'Q' then 'queen'
+        when 'K' then 'king'
+        when 'A' then 'ace'
+      end
+    end
+    "<img src='/images/cards/#{suit}_#{value}.jpg' class='card_image'>"
+  end
+end
+
+before do
+  @show_hit_or_stay_buttons = true
+end
+
 get '/clear_session' do
   session[:player_name] = nil
   redirect '/'
@@ -21,6 +66,10 @@ get '/new_player' do
 end
 
 post '/new_player' do
+  if params[:player_name].empty?
+    @error = "Name is required"
+    halt erb(:new_player)
+  end
   session[:player_name] = params[:player_name].capitalize
   redirect '/game'
 end
@@ -38,5 +87,25 @@ get '/game' do
     session[:player_cards] << session[:deck].pop
   end
 
+  erb :game
+end
+
+post '/game/player/hit' do
+  session[:player_cards] << session[:deck].pop
+
+  player_total = calculate_total(session[:player_cards])
+  if player_total == 21
+    @success = "Congrats, #{session[:player_name]} has BLACKJACK!!!"
+    @show_hit_or_stay_buttons = false
+  elsif player_total > 21
+    @error = "Sorry, it looks like #{session[:player_name]} busted. Dealer Gabby wins."
+    @show_hit_or_stay_buttons = false
+  end
+  erb :game
+end
+
+post '/game/player/stay' do
+  @success = "#{session[:player_name]} has chosen to stay.  Dealer Gabby's turn."
+  @show_hit_or_stay_buttons = false
   erb :game
 end
